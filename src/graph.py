6dec58a6e429371899f9ef97288910f484192f46
adrianmarino <adrianmarino@gmail.com>
2021-12-08 10:51:07 -0300

    feat: simplify graph visializacion and load unweighted graph

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-
+from adjacency_matrix import plot_adjacency_matrix
 
 def plot_graph(
     graph, 
@@ -27,3 +27,23 @@ def plot_graph(
         node_size   = node_size 
     )
     plt.title(title)
+
+    
+def graph_edges(graph): return graph.edges(data=True)
+    
+def graph_summary(
+    graph, 
+    title='Grafo de palabras',
+    font_color  = 'tomato',
+    font_weight = 'bold'
+):
+    print(nx.info(graph))
+    print('Is weigthed:',nx.is_weighted(graph))
+    plot_adjacency_matrix(graph)
+    plot_graph(
+        graph, 
+        title=title,
+        font_color  = font_color,
+        font_weight = font_weight,
+        node_color  = [v for v in nx.degree_centrality(graph).values()]
+    )
