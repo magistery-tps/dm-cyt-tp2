3e88bd0d307bc35a462e5023834c11a407835a66
adrianmarino <adrianmarino@gmail.com>
2021-12-11 15:15:14 -0300

    feat: rename steps and remove one node cicles

--- src/step/__init__.py
+++ src/step/__init__.py
@@ -1,3 +1,3 @@
 from .output_step import * 
 from .step_decorator import *
-from .transform_step import *
+from .filter_map_step import *
--- src/step/filter_map_step.py
+++ src/step/filter_map_step.py
@@ -21,16 +21,19 @@ def n_top(df, top): return df[:top]
 def rename_columns(df, columns):
     return df.rename(columns=columns)
 
-def filter_column_words_lt(df, column, lt=2): 
+def exclude_column_words_lt(df, column, lt=2): 
     return df.drop(df[df[column].str.len().lt(lt)].index)
 
 @step_logger
-def filter_words_lt(df, size_less_than):
-    df = filter_column_words_lt(df,  'source', size_less_than)
-    return filter_column_words_lt(df,  'response', size_less_than)
+def exclude_words_lt(df, size_less_than):
+    df = exclude_column_words_lt(df,  'source', size_less_than)
+    return exclude_column_words_lt(df,  'response', size_less_than)
 
 @step_logger
-def filter_bidiredtional_associations(df):
+def exclude_loops(df): return df[df.source != df.response]
+
+@step_logger
+def exclude_bidiredtional_associations(df):
     df   = df.drop( df[ df['response'].isin(df['source'].unique()  ) == False ].index)
     return df.drop( df[ df['source'  ].isin(df['response'].unique()) == False ].index)
 
@@ -38,14 +41,14 @@ def filter_bidiredtional_associations(df):
 def dropna(df): return df.dropna()
 
 @step_logger
-def filter_stopwords(df, column, languages=stopwords.fileids()):
+def exclude_stopwords(df, column, languages=stopwords.fileids()):
     if len(languages) == 1:
         stop_words = stopwords.words(languages[0])
         return df.drop(df[df[column].isin(stop_words)].index)        
 
     for language in languages:
-        df = filter_stopwords(df, column, languages=[language])
-    
+        df = exclude_stopwords(df, column, languages=[language])
+
     return df
 
 @step_logger
